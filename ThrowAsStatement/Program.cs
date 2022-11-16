830718b5f784bb8b84c466079e510bd5cd66e9e5
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:46:09 +0000

    Add project files.

---
+++ ThrowAsStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ThrowAsStatement
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var gen = new Throw();
+            int index = 10;
+
+            try
+            {
+                int value = gen.GetNumber(index);
+                Console.WriteLine($"Retrieved {value}");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"{e.GetType().Name}: {index} is outside the bounds of the array");
+            }
+        }
+    }
+    // The example displays the following output:
+    //        IndexOutOfRangeException: 10 is outside the bounds of the array
+}
---
+++ ThrowAsStatement/Throw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ThrowAsStatement
+{
+    class Throw
+    {
+        int[] numbers =
+        {
+            2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+            };
+
+        public int GetNumber(int index)
+        {
+            if (index < 0 || index >= numbers.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return numbers[index];
+        }
+    }
+}
